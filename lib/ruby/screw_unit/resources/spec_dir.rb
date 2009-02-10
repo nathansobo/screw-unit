d8d1e39cc1b9cc6ef41b0c69f2ff1baf36d54922
Wil Bierbaum & Nathan Sobo <mmol@grockit.com>
2009-02-10 09:31:20 -0800

    SpecDir returns SpecDirs for subdirectories and SpecSuites for unadorned spec files with when referred to without .js extension

--- lib/ruby/screw_unit/resources/spec_dir.rb
+++ lib/ruby/screw_unit/resources/spec_dir.rb
@@ -1,6 +1,25 @@
 module ScrewUnit
   module Resources
     class SpecDir < Dir
+      def locate(name)
+        relative_child_path = "#{relative_path}/#{name}".gsub("//", "/")
+        absolute_child_path = "#{absolute_path}/#{name}".gsub("//", "/")
+
+        if ::File.exists?(absolute_child_path)
+          if ::File.directory?(absolute_child_path)
+            SpecDir.new(relative_child_path, absolute_child_path)
+          else
+            File.new(relative_child_path, absolute_child_path)
+          end
+
+        elsif ::File.exists?(absolute_child_path + ".js")
+          SpecSuite.new([File.new(relative_child_path + ".js", absolute_child_path + ".js")])
+        else
+          FileNotFound.new(relative_child_path)
+        end
+
+      end
+
       def get
         SpecSuite.new(glob("/**/*.js")).get
       end
--- spec/ruby/resources/spec_dir_spec.rb
+++ spec/ruby/resources/spec_dir_spec.rb
@@ -10,6 +10,32 @@ module ScrewUnit
         @spec_dir = SpecDir.new("/specs", "#{dir}/file_system_fixtures/specs")
       end
 
+      describe "#locate" do
+        describe "when called with the name of a directory" do
+          it "returns a SpecDir for that directory" do
+            spec_subdir = spec_dir.locate("subsuite")
+            spec_subdir.class.should == SpecDir
+            spec_subdir.relative_path.should == "/specs/subsuite"
+            spec_subdir.absolute_path.should == "#{spec_dir.absolute_path}/subsuite"
+          end
+        end
+
+        describe "when called with the name of a .js file excluding the extension" do
+          it "returns a SpecSuite with the .js file as its only spec_file_resource" do
+            spec_suite = spec_dir.locate("foo_spec")
+            spec_suite.class.should == SpecSuite
+            spec_file_resources = spec_suite.spec_file_resources
+            spec_file_resources.length.should == 1
+            spec_file_resources.first.relative_path.should == "/specs/foo_spec.js"
+            spec_file_resources.first.absolute_path.should == "#{spec_dir.absolute_path}/foo_spec.js"
+          end
+        end
+
+        describe "when called with the name of a .js file including the extension" do
+          it "returns a normal File resource for the File"
+        end
+      end
+
       describe "#get" do
         it "returns the results of #get called on a SpecSuite instantiated with all /**/*.js files in the directory" do
           spec_files = spec_dir.glob("/**/*.js")
