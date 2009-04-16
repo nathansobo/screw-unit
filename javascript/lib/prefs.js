c145014c61c17febdb2dbce97b764ac0cda6a87b
Bob Remeika & Jan Nelson <grockit@grockit-03.(none)>
2009-04-16 15:25:18 -0700

    add guard against setting an invalid value for the preferences cookie

--- javascript/lib/prefs.js
+++ javascript/lib/prefs.js
@@ -10,7 +10,7 @@ var Prefs = {
         prefs_string = split_cookies[i].replace(/^ *screw_unit_prefs=/, "");
       }
     }
-    
+
     if (prefs_string) {
       this.data = JSON.parse(unescape(prefs_string));
     }
@@ -18,9 +18,10 @@ var Prefs = {
   },
 
   save: function (path, expires) {
+    if (!this.data) return;
+
     var p = path || '/';
     var d = expires || new Date(2020, 02, 02);
-
     var cookie_string = "screw_unit_prefs=" + escape(JSON.stringify(this.data)) + ';path=' + p + ';expires=' + d.toUTCString()
     document.cookie = cookie_string;
   }
