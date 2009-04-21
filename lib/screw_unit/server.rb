676be5fb701a21d2289b6151f1376100db7dac2f
Jan Nelson & Nathan Sobo <grockit@grockit-03.(none)>
2009-04-21 16:48:15 -0700

    Server#start will open a browser with selenium and point it at /specs if :selenium option is true, then kills selenium driver at_exit

--- lib/screw_unit/server.rb
+++ lib/screw_unit/server.rb
@@ -1,3 +1,5 @@
+require "selenium"
+
 module ScrewUnit
   class Server
     def self.start(options)
@@ -10,10 +12,26 @@ module ScrewUnit
       code_under_test_path = options.delete(:code_under_test_path)
       specs_path = options.delete(:specs_path)
 
+      open_suite_with_selenium(port) if options[:selenium]
+
       $thin_server = Thin::Server.new(port) do
         run Dispatcher.instance(screw_unit_core_path, code_under_test_path, specs_path, options)
       end
       $thin_server.start
+    end
+
+    def open_suite_with_selenium(port)
+      selenium_driver = Selenium::SeleniumDriver.new("localhost", 4444, "*firefox", "http://localhost:#{port}/")
+
+      Thread.new do
+        sleep 1
+        selenium_driver.start
+        selenium_driver.open("/specs")
+      end
+
+      at_exit do
+        selenium_driver.stop
+      end
     end
   end
 end
