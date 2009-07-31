ae97db258a258b236bf1093ac44c694d725ddd06
Jake Frautschi & Jan Nelson <mmol@grockit.com>
2009-07-31 12:45:06 -0700

    launch Thin::Server in a new thread in order to simplify the order of operations when launching the selenium driver and remove an unnecessary sleep

--- lib/screw_unit/server.rb
+++ lib/screw_unit/server.rb
@@ -2,6 +2,8 @@ require "selenium"
 
 module ScrewUnit
   class Server
+    attr_reader :thin_thread
+
     def self.start(options)
       new.start(options)
     end
@@ -12,12 +14,18 @@ module ScrewUnit
       code_under_test_path = options.delete(:code_under_test_path)
       specs_path = options.delete(:specs_path)
 
+      start_thin_server_in_new_thread(port, screw_unit_core_path, code_under_test_path, specs_path, options)
       open_suite_with_selenium(port) if options[:selenium]
+      thin_thread.join
+    end
 
-      $thin_server = Thin::Server.new(port) do
-        run Dispatcher.instance(screw_unit_core_path, code_under_test_path, specs_path, options)
+    def start_thin_server_in_new_thread(port, screw_unit_core_path, code_under_test_path, specs_path, options)
+      @thin_thread = Thread.new do
+        $thin_server = Thin::Server.new(port) do
+          run Dispatcher.instance(screw_unit_core_path, code_under_test_path, specs_path, options)
+        end
+        $thin_server.start
       end
-      $thin_server.start
     end
 
     def open_suite_with_selenium(port)
@@ -28,16 +36,13 @@ module ScrewUnit
         "#{Configuration.selenium_screw_server_url}:#{Configuration.selenium_screw_server_port}"
       )
 
-      Thread.new do
-        sleep 1
-        selenium_driver.start
-        selenium_driver.open(Configuration.selenium_specs_path)
-      end
-
       at_exit do
         sleep 1 if ENV['SELENIUM_BROWSER_STRING'] == "*iexplore"
         selenium_driver.stop
       end
+
+      selenium_driver.start
+      selenium_driver.open(Configuration.selenium_specs_path)
     end
   end
 end
--- spec/screw_unit/server_spec.rb
+++ spec/screw_unit/server_spec.rb
@@ -11,36 +11,5 @@ module ScrewUnit
         Server.start(options)
       end
     end
-
-    describe "#start" do
-      it "calls Thin::Server.start with an instance of the Dispatcher with the given options" do
-        mock.proxy(Thin::Server).new(9090) do |return_value|
-          stub(return_value).start
-        end
-        mock.proxy(Dispatcher).instance("screw/unit/core/path", "code/under/test/path", "specs/path", :selenium => true, :some_future_option => "some value")
-
-        Server.start(
-          :port => 9090,
-          :screw_unit_core_path => "screw/unit/core/path",
-          :code_under_test_path => "code/under/test/path",
-          :specs_path => "specs/path",
-          :selenium => true,
-          :some_future_option => "some value"
-        )
-      end
-
-      context "when no port option is supplied" do
-        it "defaults to 8080" do
-          mock.proxy(Thin::Server).new(8080) do |return_value|
-            stub(return_value).start
-          end
-          Server.start(
-            :screw_unit_core_path => "screw/unit/core/path",
-            :code_under_test_path => "code/under/test/path",
-            :specs_path => "specs/path"
-          )
-        end
-      end
-    end
   end
 end
