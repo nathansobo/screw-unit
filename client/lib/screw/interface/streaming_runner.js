0a996bfb70fbd3bb9dd6ab2a729fc62da6d5b080
Grockit <grockit@grok-a.local>
2009-10-09 13:03:03 -0700

    Added total specs count to streaming runner and cleaned it up a bit

--- client/lib/screw/interface/streaming_runner.js
+++ client/lib/screw/interface/streaming_runner.js
@@ -11,17 +11,22 @@
     content: function() { with(this.builder) {
       div(function() {
         div(function() {
-          span("Spec Files: ")
-          span().ref('completed_files_span');
+          span("Total Examples: ");
+          span().ref('num_completed_examples_span');
+        });
+        
+        div(function() {
+          span("Files: ")
+          span().ref('num_completed_files_span');
           span(" / ")
           span().ref('total_files_span');
         });
 
         div(function() {
           span("Examples In Current File: ")
-          span().ref('completed_examples_span');
+          span().ref('completed_examples_in_current_file_span');
           span(" / ")
-          span().ref('total_examples_span');
+          span().ref('total_examples_in_current_file_span');
         });
 
         ul(function() {
@@ -34,8 +39,10 @@
 
     view_properties: {
       initialize: function() {
-        this.num_completed_spec_files = 0;
-        this.completed_files_span.html("0");
+        this.num_completed_examples = 0;
+        this.num_completed_files = 0;
+        this.num_completed_examples_span.html("0");
+        this.num_completed_files_span.html("0");
         this.total_files_span.html(this.spec_paths.length);
         this.load_next_spec_path();
       },
@@ -53,15 +60,17 @@
         var self = this;
         var root = Screw.root_description();
         var num_examples = root.total_examples();
-        var num_examples_completed = 0;
-        self.completed_examples_span.html("0");
-        self.total_examples_span.html(num_examples);
+        var num_completed_examples_in_current_file = 0;
+        self.completed_examples_in_current_file_span.html("0");
+        self.total_examples_in_current_file_span.html(num_examples);
 
         Screw.root_description().on_example_completed(function(example) {
-          num_examples_completed++;
-          self.completed_examples_span.html(num_examples_completed);
+          self.num_completed_examples++;
+          self.num_completed_examples_span.html(self.num_completed_examples);
+          num_completed_examples_in_current_file++;
+          self.completed_examples_in_current_file_span.html(num_completed_examples_in_current_file);
           if (!example.passed) self.handle_failed_example(example);
-          if (num_examples_completed == num_examples) self.examples_completed_for_current_spec_file();
+          if (num_completed_examples_in_current_file == num_examples) self.examples_completed_for_current_spec_file();
         });
 
         Screw.root_description().enqueue()
@@ -78,8 +87,8 @@
 
       examples_completed_for_current_spec_file: function() {
         Screw.clear_descriptions();
-        this.num_completed_spec_files++;
-        this.completed_files_span.html(this.num_completed_spec_files);
+        this.num_completed_files++;
+        this.num_completed_files_span.html(this.num_completed_files);
         this.load_next_spec_path();
       }
     }
