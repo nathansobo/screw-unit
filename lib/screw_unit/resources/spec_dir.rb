7a5cbe0757a6b798443024bf454cc2fd27996236
Nathan Sobo <nathansobo@gmail.com>
2010-04-06 06:47:54 -0600

    Fixed exception when an indicated spec directory is not found. Now just returns 404.

--- lib/screw_unit/resources/spec_dir.rb
+++ lib/screw_unit/resources/spec_dir.rb
@@ -9,13 +9,13 @@ module ScrewUnit
         virtual_child_path = ::File.join(virtual_path, name)
         physical_child_path = asset_manager.physicalize_path(virtual_child_path)
 
-        if ::File.exists?(physical_child_path)
+        if physical_child_path && ::File.exists?(physical_child_path)
           if ::File.directory?(physical_child_path)
             SpecDir.new(virtual_child_path, asset_manager)
           else
             File.new(virtual_child_path, asset_manager)
           end
-        elsif ::File.exists?(physical_child_path + ".js")
+        elsif physical_child_path && ::File.exists?(physical_child_path + ".js")
           SpecRunner.new([File.new(virtual_child_path + ".js", asset_manager)], asset_manager)
         else
           FileNotFound.new(virtual_child_path)
